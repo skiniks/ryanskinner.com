3f9cf1a1735df7204cd1e7eea62f2ab8c4a821c6
Ryan Skinner <ryan@officebureau.ca>
2022-12-10 17:54:37 -0500

    fixing aspect ratio

--- components/Swiper.vue
+++ components/Swiper.vue
@@ -9,9 +9,9 @@
 						loading="lazy"
 						src="/images/uoft-logo.svg"
 						alt="University of Toronto"
-						sizes="xs:280px md:480px lg:600px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:600px"
+						width="225"
+						height="125"
 					/>
 				</div>
 				<div class="swiper-slide">
@@ -21,9 +21,9 @@
 						loading="lazy"
 						src="/images/wgc-logo.png"
 						alt="Wine Growers Canada"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -34,9 +34,9 @@
 						loading="lazy"
 						src="/images/c3-logo.png"
 						alt="Cannabis Council of Canada"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -47,9 +47,9 @@
 						loading="lazy"
 						src="/images/rhf-logo.png"
 						alt="Rideau Hall Foundation"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -60,9 +60,9 @@
 						loading="lazy"
 						src="/images/cbtu-logo.svg"
 						alt="Canadian Building Trades Union"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -73,9 +73,9 @@
 						loading="lazy"
 						src="/images/nwac-logo.png"
 						alt="Native Women's Association of Canada"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -86,9 +86,9 @@
 						loading="lazy"
 						src="/images/calpoly-logo.png"
 						alt="California Polytechnic State University"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -99,9 +99,9 @@
 						loading="lazy"
 						src="/images/reed-logo.png"
 						alt="Reed College, Portland, Oregon"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -112,9 +112,9 @@
 						loading="lazy"
 						src="/images/carthage-logo.png"
 						alt="Carthage College, Kenosha, Wisconsin"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -125,9 +125,9 @@
 						loading="lazy"
 						src="/images/ppmd-logo.png"
 						alt="Parent Project Muscular Dystrophy"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -138,9 +138,9 @@
 						loading="lazy"
 						src="/images/eoc-logo.png"
 						alt="Energy Outreach Colorado"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -151,9 +151,9 @@
 						loading="lazy"
 						src="/images/wayfinder-logo.png"
 						alt="Wayfinder Family Services"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -164,9 +164,9 @@
 						loading="lazy"
 						src="/images/aicf-logo.png"
 						alt="American Indian College Fund"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -177,9 +177,9 @@
 						loading="lazy"
 						src="/images/recording-academy.png"
 						alt="Recording Academy"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -190,9 +190,9 @@
 						loading="lazy"
 						src="/images/cal-state-seal.png"
 						alt="California State University"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
@@ -203,9 +203,9 @@
 						loading="lazy"
 						src="/images/mcmaster-wordmark.png"
 						alt="McMaster University"
-						sizes="sm:280px md:480px lg:680px"
-						width="280"
-						height="150"
+						sizes="xs:225px md:480px lg:680px"
+						width="225"
+						height="125"
 						fit="contain"
 					/>
 				</div>
