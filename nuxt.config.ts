e16df7e72978a7b6c95d1ffe50f812ede8f55811
Ryan Skinner <ryan@officebureau.ca>
2022-12-10 18:27:09 -0500

    removing target, not applicable in 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	ssr: false,
-	target: "static",
 	components: true,
 	modules: [
 		// https://windicss.org/guide/nuxt.html
@@ -11,10 +10,8 @@ export default defineNuxtConfig({
 		// https://github.com/nuxt-community/robots-module
 		"@nuxtjs/robots",
 	],
-	buildModules: ["@nuxt/typescript-build"],
-	"@nuxt/typescript-build": {
+	typescript: {
 		strict: true,
-		typeCheck: true,
 	},
 	css: ["virtual:windi-base.css", "virtual:windi-utilities.css"],
 })
