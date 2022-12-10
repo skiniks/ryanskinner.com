37b7831f027ddae1cc2cf2ae27c4d078c737cae3
Ryan Skinner <ryan@officebureau.ca>
2022-12-10 14:50:52 -0500

    correcting animation class

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,7 +6,7 @@
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 562.21 183.47"
-					class="logo hover:animate-heartbeat"
+					class="logo hover:animate-heartBeat"
 				>
 					<path
 						fill="#fff"
