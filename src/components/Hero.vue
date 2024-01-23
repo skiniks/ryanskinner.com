2f46e0fd458caf7dfc9b653e5a0932a203605b3e
Ryan Skinner <ryanskinner@gmail.com>
2024-01-23 14:14:17 -0500

    better sizing for mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -23,7 +23,7 @@
           <h1 class="text-6xl font-bold tracking-tight text-transparent bg-clip-text animated-gradient sm:text-8xl pb-2">
             Let's build a better internet together.
           </h1>
-          <p class="mt-6 text-lg leading-8 text-gray-300 font-mono">
+          <p class="mt-6 text-lg leading-8 text-gray-300 font-mono sm:text-xl">
             I'm a creative software developer passionate about exceptional
             design, enhancing the developer experience, and pushing the boundaries of the modern web. I've collaborated
             with a diverse range of clients and utilized various technologies to craft beautiful, accessible, and
