d37147d25a16cc91939ac657bef61ad9f8212969
Ryan Skinner <ryanskinner@gmail.com>
2024-01-25 14:46:39 -0500

    further og edit

--- src/components/OgImage/Og.vue
+++ src/components/OgImage/Og.vue
@@ -1,21 +1,8 @@
-<script setup lang="ts">
-withDefaults(defineProps<{
-  title?: string
-}>(), {
-  title: 'title',
-})
-</script>
-
 <template>
-  <div class="h-full w-full flex items-start justify-start border-solid border-blue-500 border-[12px] bg-gray-50">
-    <div class="flex items-start justify-start h-full">
-      <div class="flex flex-col justify-between w-full h-full">
-        <h1 class="text-[80px] p-20 font-black text-left">
-          {{ title }}
-        </h1>
-        <p class="text-2xl pb-10 px-20 font-bold mb-0">
-          mycoolsite.com
-        </p>
+  <div class="h-full w-full flex items-center justify-center bg-gray-800">
+    <div class="flex items-center justify-center h-full">
+      <div class="flex flex-col justify-center items-center w-full h-full">
+        <img src="/logo.svg" alt="og-image" class="w-1/2" />
       </div>
     </div>
   </div>
