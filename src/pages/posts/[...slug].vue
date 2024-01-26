a82b40c12b30681d65d196d6c6167e38b12ce6de
Ryan Skinner <ryanskinner@gmail.com>
2024-01-26 17:29:56 -0500

    removing second useAsyncData

--- src/pages/posts/[...slug].vue
+++ src/pages/posts/[...slug].vue
@@ -1,50 +1,42 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute, createError } from 'nuxt/app'
+
 definePageMeta({
   layout: 'article',
 })
 
 const route = useRoute()
+const path = computed(() => route.path.replace(/(index)?\.json$/, '').replace(/\/$/, ''))
 
-const path = computed(() =>
-  route.path.replace(/(index)?\.json$/, '').replace(/\/$/, ''),
-)
+const { data: pageData, error } = await useAsyncData('pageData', () => {
+  const contentQuery = queryContent(path.value)
+    .only(['title', 'date', 'tags', 'description'])
 
-const { data: page } = await useAsyncData(
-  path.value,
-  () =>
-    ((import.meta.server || import.meta.dev) as true) &&
-    queryContent(path.value)
-      .only(['title', 'date', 'tags', 'description'])
-      .findOne()
-)
+  return contentQuery.findOne()
+})
 
-if (!page.value) {
+if (error.value || !pageData.value) {
   throw createError({
     statusCode: 404,
     fatal: true,
   })
 }
 
-route.meta.title = page.value.title
-route.meta.description = page.value.description
-
-const { data: contentData } = await useAsyncData('content', () =>
-  queryContent()
-    .where({ _path: path.value })
-    .only(['title', 'date', 'tags'])
-    .findOne())
+route.meta.title = pageData.value.title
+route.meta.description = pageData.value.description
 
 defineOgImageComponent('NuxtSeo', {
-  title: page.value.title,
+  title: pageData.value.title,
 })
 </script>
 
 <template>
   <header class="mt-8 px-4 lg:px-0">
-    <NuxtTime :datetime="contentData?.date" day="numeric" month="long" year="numeric" />
-    <h1>{{ contentData?.title }}</h1>
+    <NuxtTime :datetime="pageData?.date" day="numeric" month="long" year="numeric" />
+    <h1>{{ pageData?.title }}</h1>
     <ul class="list-none flex flex-wrap gap-2 p-0">
-      <li v-for="tag in contentData?.tags" :key="tag"
+      <li v-for="tag in pageData?.tags" :key="tag"
         class="bg-blue-200 text-blue-800 light:bg-blue-800 light:text-blue-200 text-sm font-semibold py-1 px-3 rounded-full">
         {{ tag }}
       </li>
