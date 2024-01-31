85696a20b783e3997fb5424cabd4e2d6dd1b20fd
Ryan Skinner <ryanskinner@gmail.com>
2024-01-31 15:12:40 -0500

    adding meta description

--- src/app.vue
+++ src/app.vue
@@ -4,6 +4,13 @@ const route = useRoute()
 useHead({
   title: () => (route.meta.title as string) || '',
   titleTemplate: title => (title ? `${title} - Ryan Skinner` : 'Ryan Skinner'),
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Ryan Skinner is a software engineer and web developer based in Toronto, Canada.'
+    }
+  ]
 })
 </script>
 
