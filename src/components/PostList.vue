46685fdd062dd40e6b45213bc22fbeb63c47cdf8
Ryan Skinner <ryanskinner@gmail.com>
2024-02-01 11:24:30 -0500

    responsive fix

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -13,27 +13,27 @@ const entries = await queryContent('/posts')
 </script>
 
 <template>
-  <div class="mx-auto max-w-7xl px-2 lg:px-0">
-    <div class="mx-auto max-w-2xl">
-      <div class="space-y-16">
+  <div class="mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-4xl">
+      <div class="space-y-8">
         <article
           v-for="{ title, date, description, path } in entries" :key="path"
-          class="flex max-w-3xl flex-col items-start justify-between bg-gray-900 light:bg-gray-800/80 rounded-lg px-12 py-14 my-4 shadow-md transition-all ease-in-out duration-300 hover:shadow-lg animate-fadeInMoveUp"
+          class="flex flex-col items-start justify-between bg-gray-900 light:bg-gray-800/80 rounded-lg px-6 py-8 sm:px-8 sm:py-10 md:px-12 md:py-14 my-4 shadow-md transition-all ease-in-out duration-300 hover:shadow-lg animate-fadeInMoveUp"
         >
-          <div class="mb-4">
+          <div class="mb-2 sm:mb-4">
             <span class="bg-blue-600 text-white text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full">
               <NuxtTime :datetime="date" day="numeric" month="long" year="numeric" />
             </span>
           </div>
-          <h3 class="text-5xl font-bold text-gray-100 leading-tight mb-4">
+          <h3 class="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-100 leading-tight mb-3 sm:mb-4">
             <a :href="path" class="hover:text-gray-300 transition-colors duration-200">
               {{ title }}
             </a>
           </h3>
-          <p class="text-white text-lg leading-relaxed mb-10">
+          <p class="text-white text-base sm:text-lg leading-relaxed mb-6 sm:mb-10">
             {{ description }}
           </p>
-          <a :href="path" class="text-white bg-blue-600 hover:bg-blue-800 px-4 py-4 rounded-xl" :aria-label="'Read more about ' + title" :title="'Read more about ' + title">
+          <a :href="path" class="text-white bg-blue-600 hover:bg-blue-800 px-3 sm:px-4 py-2 sm:py-3 rounded-xl" :aria-label="'Read more about ' + title" :title="'Read more about ' + title">
             View Post
           </a>
         </article>
--- src/components/RecentPost.vue
+++ src/components/RecentPost.vue
@@ -14,27 +14,35 @@ const entries = await queryContent('/posts')
 </script>
 
 <template>
-  <div class="flex justify-center items-center min-h-screen">
-    <div v-if="entries.length > 0" :key="entries[0].path" class="max-w-3xl px-12 py-14 bg-gray-900 light:bg-gray-800/80 rounded-lg">
-      <div class="mb-4 text-white text-sm">
-        <span class="bg-blue-600 text-white text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full">
-          <NuxtTime :datetime="entries[0].date" day="numeric" month="long" year="numeric" />
-        </span>
+  <div class="p-8 md:p-12">
+  <div class="mx-auto px-4 sm:px-6 lg:px-8 min-h-screen">
+    <div class="mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-4xl">
+      <div class="space-y-8">
+        <div v-if="entries.length > 0" :key="entries[0].path" class="flex flex-col items-start justify-between bg-gray-900 light:bg-gray-800/80 rounded-lg px-6 py-8 sm:px-8 sm:py-10 md:px-12 md:py-14 my-4 shadow-md transition-all ease-in-out duration-300 hover:shadow-lg animate-fadeInMoveUp">
+          <div class="mb-4">
+            <span class="bg-blue-600 text-white text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full">
+              <NuxtTime :datetime="entries[0].date" day="numeric" month="long" year="numeric" />
+            </span>
+          </div>
+          <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-100 leading-tight mb-3 sm:mb-4">
+            <a :href="entries[0].path" class="hover:text-gray-300 transition-colors duration-200">
+              {{ entries[0].title }}
+            </a>
+          </h1>
+          <p class="text-white text-lg leading-relaxed mb-10">
+            {{ entries[0].description }}
+          </p>
+          <a :href="entries[0].path" class="text-white bg-blue-600 hover:bg-blue-800 px-4 py-4 rounded-xl" :aria-label="'Read more about ' + entries[0].title" :title="'Read more about ' + entries[0].title">
+            View Post
+          </a>
+        </div>
+        <div v-else class="text-center">
+          <p>No recent posts available.</p>
+        </div>
       </div>
-      <h1 class="mb-4 text-5xl font-bold text-white leading-tight">
-        <a :href="entries[0].path" class="hover:text-gray-300 transition-colors duration-200">
-          {{ entries[0].title }}
-        </a>
-      </h1>
-      <p class="text-white text-lg leading-relaxed mb-10">
-        {{ entries[0].description }}
-      </p>
-      <a :href="entries[0].path" class="text-white bg-blue-600 hover:bg-blue-800 px-4 py-4 rounded-xl" :aria-label="'Read more about ' + entries[0].title" :title="'Read more about ' + entries[0].title">
-      View Post
-      </a>
-    </div>
-    <div v-else class="text-center">
-      <p>No recent posts available.</p>
     </div>
   </div>
+</div>
 </template>
+
+
