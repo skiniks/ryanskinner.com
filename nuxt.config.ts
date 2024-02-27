4022e32fbc84e5aec48636a5dc44d7fddc49c29c
Ryan Skinner <ryanskinner@gmail.com>
2024-02-27 12:56:24 -0500

    adding bundler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,10 @@ export default defineNuxtConfig({
     typedPages: true,
   },
 
+  future: {
+    typescriptBundlerResolution: true,
+  },
+
   modules: [
     '@unocss/nuxt',
     '@nuxtjs/color-mode',
