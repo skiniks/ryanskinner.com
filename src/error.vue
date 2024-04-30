349ba3fffb6103196618bb0865686f28bc851d04
Ryan Skinner <ryan@42north.ca>
2024-04-30 10:53:10 -0400

    adding error page

---
+++ src/error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+  <NuxtLayout>
+    <img
+      v-if="error.statusCode === 404"
+      src="/images/notfound.png"
+      alt="Page Not Found"
+      class="max-w-4xl mx-auto"
+    >
+    <h1 v-else>
+      An error occurred
+    </h1>
+  </NuxtLayout>
+</template>
